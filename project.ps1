cd30d1cba9abf81e3a03f6ebd0326d4d06d5d03d
lmascelli <40148338+lmascelli@users.noreply.github.com>
2023-09-15 18:28:04 +0200

    remove init from project.ps1

--- project.ps1
+++ project.ps1
@@ -24,21 +24,7 @@ switch ($args[0]) {
     build
       run $args[1]
   }
-
-  "init" {
-    $init_folder = "new"
-      if ($args.Length -ge 2) {
-        $init_folder = $args[1]
-      }
-    New-Item -Type Directory -Path $init_folder
-      Copy-Item -Path ./cmake -Recurse -Destination $init_folder
-      Copy-Item -Path ./src -Recurse -Destination $init_folder
-      Copy-Item -Path ./build -Recurse -Destination $init_folder
-      Copy-Item -Path ./CMakeLists.txt -Destination $init_folder
-      Copy-Item -Path ./project.ps1 -Destination $init_folder
-      Copy-Item -Path ./.gitignore -Destination $init_folder
-  }
-
+  
   default {
     run
   }
