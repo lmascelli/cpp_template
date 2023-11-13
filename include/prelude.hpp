881f96232f73aa4a51508587035b763530831988
lmascelli <lmascelli@gmail.com>
2023-11-13 09:32:14 +0100

    richer prelude

--- include/prelude.hpp
+++ include/prelude.hpp
@@ -6,5 +6,26 @@ using std::array;
 #include <vector>
 using std::vector;
 
+#include <utility>
+using std::pair;
+using std::tuple;
+
 #include <iostream>
-using std::cout, std::cin, std::endl;
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::endl;
+
+#include <functional>
+using std::function;
+
+#if __cplusplus >= 202002L
+#include <ranges>
+using namespace std::ranges;
+
+#include <optional>
+using std::optional, std::nullopt;
+
+#include <variant>
+using std::variant;
+#endif
